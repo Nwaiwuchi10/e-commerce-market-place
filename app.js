d0312f46624c55284ea6f21e19e5c35aa94148bb
Nwaiwuchi10 <djnchrys@gmail.com>
2024-09-02 17:07:57 -0700

    user

--- app.js
+++ app.js
@@ -45,10 +45,10 @@ mongoose
 app.use(express.json());
 app.use("/api/auth/", authRoutes);
 
-app.use(express.static(path.join(__dirname, "/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "build/index.html"))
-);
+// app.use(express.static(path.join(__dirname, "/build")));
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "build/index.html"))
+// );
 const PORT = process.env.PORT || 5000;
 
 app.listen(
